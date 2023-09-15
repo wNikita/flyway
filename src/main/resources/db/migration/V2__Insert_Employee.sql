24bcef11c6d178eb52737426ac030764e5ded8ff
Nikita <nrw@narola.email>
2023-09-15 14:36:32 +0530

    insert data using .sql file

---
+++ src/main/resources/db/migration/V2__Insert_Employee.sql
@@ -0,0 +1,4 @@
+-- V2__Insert_Employee.sql
+INSERT INTO employee (first_name, last_name) VALUES ('Nikita', 'Shinde');
+INSERT INTO employee (first_name, last_name) VALUES ('Jane', 'Smith');
+-- Add more INSERT statements as needed
