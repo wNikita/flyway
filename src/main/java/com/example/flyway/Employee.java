a54347749b7c522a2c9c62daa5c77c9e373e3152
Nikita <nrw@narola.email>
2023-09-15 15:28:16 +0530

    insert data using .sql file

--- src/main/java/com/example/flyway/Employee.java
+++ src/main/java/com/example/flyway/Employee.java
@@ -13,5 +13,27 @@ public class Employee {
     private String firstName;
     private String lastName;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
